f3d861240fd913815f2c1e1b3345e2c510b3801a
miky-rola <mikyrola8@gmail.com>
2025-02-22 11:19:13 +0000

    updated the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use rayon::prelude::*;
 
-fn parralel_sum(numbers: &[i32]) -> i32{
-
+fn parrelel_sum(numbers: &[i32]) -> i32 {
     if numbers.is_empty(){
-        return  0;
+        return 0;
     }
+
     numbers.par_iter().sum()
 }
 
 fn main(){
-    
+    let numbers: Vec<i32> = (1..200).collect();
+
+    let seq_sum: i32 = numbers.iter().sum();
+
+    let parellel_results = parrelel_sum(&numbers);
+
+    println!("THIS IS THE SEQUENTIAL NUMBERS: {}", seq_sum);
+    println!("THIS IS THE PARALLEL NUMBERS: {}", parellel_results);
+
+    assert_eq!(seq_sum, parellel_results)
 }
