4102e0e431c38251b9057d5c00b980c476978d07
miky-rola <mikyrola8@gmail.com>
2025-02-22 10:42:27 +0000

    updated the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rayon::prelude::*;
+
+fn parralel_sum(numbers: &[i32]) -> i32{
+
+    if numbers.is_empty(){
+        return  0;
+    }
+    numbers.par_iter().sum()
+}
+
+fn main(){
+    
 }
